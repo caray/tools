bbaa48d04f6246dce3d00a20367aac79b008839f
caray <caray@foxmail.com>
2020-10-21 14:01:44 +0800

    first commit

---
+++ centosInit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+#=================================================
+#	System Required: CentOS 6/7
+#	Description: 系统初始化
+#	Version: 1.0
+#	Author: caray
+#=================================================
+
+sh_ver="1.0"
+github="raw.githubusercontent.com/caray/tools/master"
