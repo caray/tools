9566b194e54739885a784c6abf8867fb9ab60b1d
caray <caray@foxmail.com>
2022-10-13 13:53:17 +0800

    Update centosInit.sh

--- centosInit.sh
+++ centosInit.sh
@@ -50,3 +50,44 @@ firewall-cmd --zone=public --add-port=443/tcp --permanent
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 firewall-cmd --reload
 
+#安装密钥
+if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
+	echo "Info: ~/.ssh/authorized_keys is missing ...";
+
+	echo "Creating ${HOME}/.ssh/authorized_keys ..."
+	mkdir -p ${HOME}/.ssh/
+	touch ${HOME}/.ssh/authorized_keys
+
+	if [ ! -f "${HOME}/.ssh/authorized_keys" ]; then
+		echo "Failed to create SSH key file"
+	else
+		echo "Key file created, proceeding..."
+	fi
+fi
+
+#get key from server
+curl -D /tmp/headers.txt https://raw.githubusercontent.com/caray/tools/main/id_rsa.pub >/tmp/key.txt 2>/dev/null
+HTTP_CODE=$(sed -n 's/HTTP\/1\.[0-9] \([0-9]\+\).*/\1/p' /tmp/headers.txt | tail -n 1)
+if [ $HTTP_CODE -ne 200 ]; then
+	echo "Error: server went away"; exit 1;
+fi
+PUB_KEY="$(cat /tmp/key.txt)"
+
+if [ "${PUB_KEY}" = '0' ]; then
+	echo "Error: Key wasn't found"; exit 1;
+fi
+
+if [ $(grep -m 1 -c "${PUB_KEY}" ${HOME}/.ssh/authorized_keys) -eq 1 ]; then
+	echo 'Warning: Key is already installed'; exit 1;
+fi
+
+#install key
+echo -e "\n${PUB_KEY}\n" >> ${HOME}/.ssh/authorized_keys
+rm -rf /tmp/key.txt
+rm -rf /tmp/headers.txt
+echo 'Key installed successfully'
+echo 'Thanks, Key Installer'
+
+#禁止密码登录
+sed -i "s#PasswordAuthentication yes#PasswordAuthentication no#g" /etc/ssh/sshd_config
+service sshd restart
